fbf9690937781abbc76279f375ce756f9f9cbe8b
barnanicholas <barna_nicholas@tamu.edu>
2022-11-25 20:10:01 -0800

    aoeu

--- task4/task4.c
+++ task4/task4.c
@@ -28,7 +28,7 @@ int main()
 
    unsigned char message[] = "hello, this is a really cool file with some really cool contents\n";
    unsigned char ** digest;
-   //messagedigest(message, digest);
+   messagedigest(message, digest);
    printf("the digest of \'%s\' is \'%s\'\n", message, *digest);
 
    return 0; 
