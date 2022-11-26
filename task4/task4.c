0dc58eaa529d1689bae41f69c2d299d69b32a9d3
barnanicholas <barna_nicholas@tamu.edu>
2022-11-25 20:03:49 -0800

    aoeu

--- task4/task4.c
+++ task4/task4.c
@@ -6,12 +6,12 @@ int messagedigest(const unsigned char *message, unsigned char **digest)
 {
    unsigned int len_digest;
    EVP_MD_CTX *mdctx;
-	mdctx = EVP_MD_CTX_new();
+	mdctx = EVP_MD_CTX_create();
 	EVP_DigestInit_ex(mdctx, EVP_md5(), NULL);
 	EVP_DigestUpdate(mdctx, message, strlen(message));
 	*digest = (unsigned char *)OPENSSL_malloc(EVP_MD_size(EVP_md5()));
    EVP_DigestFinal_ex(mdctx, *digest, &len_digest);
-	EVP_MD_CTX_free(mdctx);
+	EVP_MD_CTX_destroy(mdctx);
 
    return len_digest;
 }
@@ -23,8 +23,8 @@ int messagedigest(const unsigned char *message, unsigned char **digest)
 
 int main()
 {
-   char message[] = "hello, this is a really cool file with some really cool contents\n";
-   char ** digest;
+   unsigned char message[] = "hello, this is a really cool file with some really cool contents\n";
+   unsigned char ** digest;
    messagedigest(message, digest);
    printf("the digest of \'%s\' is \'%s\'", message, *digest);
 
