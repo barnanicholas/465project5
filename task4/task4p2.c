c70f94a6fe801b7f20d62cf2f84d924bcb7dec74
barnanicholas <barna_nicholas@tamu.edu>
2022-11-26 10:31:40 -0800

    aoeu

--- task4/task4p2.c
+++ task4/task4p2.c
@@ -53,28 +53,16 @@ int main()
       unsigned char * digest = malloc(EVP_MAX_MD_SIZE);
 
       //while (!(digest[0] == digestmain[0] && digest[1] == digestmain[1]  && digest[2] == digestmain[2]) || message[255] == 127)
-      while (!(message[1000] == 126))
+      while (1)
       {
-         for (int i = 0; i < 1000; i++)
+         for (int i = 0; i < 1001; i++)
          {
-            if (message[i] < 126)
-            {
-               message[i] = message[i] + 1;
-               if(message[i] == 126)
-               {
-                  for (int j = 0; j < i; j++)
-                  {
-                     message[j] = 34;
-                  }
-
-               }
-               break;
-            }
-
+            message[i] = (rand() % (126 - 33 + 1)) + 33;
          }
-         //printf("message %s \n", message);
          trials += 1;
+
          messagedigest(message, digest);
+
          if (digest[0] == digestmain[0] && digest[1] == digestmain[1]  && digest[2] == digestmain[2])
          {
             printf("sucess");
@@ -83,12 +71,7 @@ int main()
 
       }
 
-      if (message[255] == 126)
-      {
-         printf("better luck next time");
-      }
-      else
-      {
+     
       printf("digest main : ");
       for (int i = 0; i < 32; i++)
          printf("%02x", digestmain[i]);
@@ -101,9 +84,6 @@ int main()
       printf("\n");
 
       printf("message main : %s \n message alt : %s \n number of trials req: %u \n", messagemain, message, trials);
-
-
-      }
       messagemain[0] += 1;
    }
    return 0;
