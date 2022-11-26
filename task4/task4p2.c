ef5bb9586270a4d50a5a8b482469fad52968a11e
barnanicholas <barna_nicholas@tamu.edu>
2022-11-26 10:32:56 -0800

    aoeu

--- task4/task4p2.c
+++ task4/task4p2.c
@@ -63,7 +63,7 @@ int main()
 
          messagedigest(message, digest);
 
-         if (digest[0] == digestmain[0] && digest[1] == digestmain[1])
+         if (digest[0] == digestmain[0] && digest[1] == digestmain[1]  && digest[2] == digestmain[2])
          {
             printf("sucess");
             break;
